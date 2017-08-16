8f644e406d0e9d9ddcc1f9f1683675ca37eaf98a
viju99 <31035012+viju99@users.noreply.github.com>
2017-08-16 17:37:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+app.get('/article-two', function (req, res) {
   res.send('Article two requested and will be served here');
 });
 
