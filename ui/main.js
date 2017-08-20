bf41d677ccf0913f348b19e5ac1ab60070047c15
viju99 <31035012+viju99@users.noreply.github.com>
2017-08-20 17:32:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function() {
+  
+  //Create a request object
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if (request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      }
+  };
+  
+  //Make a request
+  request.open('GET', 'http://viju99.imad.hasura-app.io/counter', true);
+  request.send(null);
+};
