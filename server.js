2a97906398b75ccaa456882b72f9f27440a50ebb
viju99 <31035012+viju99@users.noreply.github.com>
2017-08-27 23:43:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'viju99',
+    database: 'viju99',
+    host: 'db.imad.hasura.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -95,6 +105,20 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db',  function(req,res){
+    //make a select request
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
+    //return a response with the results
+});
+
 
 var counter = 0;
 app.get('/counter', function(req,res){
