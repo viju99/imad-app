1d3dde0f49884f73f0dadc69194bcd12de9dae62
viju99 <31035012+viju99@users.noreply.github.com>
2017-08-29 12:03:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,6 +141,34 @@ app.post('/create-user', function(req, res){
     });
 });
 
+app.post('/login', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user"  WHERE username = $1', [username], function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            if(result.rows.lengtj === 0){
+                res.send(403).sned('username/password is invalid');
+            }
+            else{
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password, salt); //creating a hash based on the pwd submitted
+                if (hashedPassword === dbString) {
+                    res.send('Credentials correct');
+                }
+                else {
+                    res.send(403).sned('username/password is invalid');
+                }
+            }
+            
+        }
+        
+    });
+});
+
+
 var pool = new Pool(config);
 app.get('/test-db',  function(req,res){
     //make a select request
