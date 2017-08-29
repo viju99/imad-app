74c222655e3b41df32e43c6cee7258552ee06be0
viju99 <31035012+viju99@users.noreply.github.com>
2017-08-29 12:21:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
 //counter code
+/*
 var button = document.getElementById('counter');
 button.onclick = function() {
   
@@ -21,3 +22,37 @@ button.onclick = function() {
   request.open('GET', 'http://viju99.imad.hasura-app.io/counter', true);
   request.send(null);
 };
+*/
+var button = document.getElementById('submit_btn');
+button.onclick = function() {
+  
+  //Create a request object
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.DONE) {
+          //Take some action
+          if (request.status === 200){
+              console.log("User logged in successfully");
+              alert('Logged in successfully');
+          }
+          else if(request.status === 403) {
+              alert('username/password is incorrect');
+          }else if(request.status === 500) {
+              alert('something went wrong on the server');
+          }
+      }
+  };
+  
+  //Make a request
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST', 'http://viju99.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username: username, password: password}));
+};
+  
+  
