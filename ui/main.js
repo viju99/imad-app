8038200b8b171c241e7013cb7dff7971881e7120
viju99 <31035012+viju99@users.noreply.github.com>
2017-08-29 12:38:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ button.onclick = function() {
   
   //Create a request object
   var request = new XMLHttpRequest();
-  
+  console.log('reached here');
   //Capture the response and store it in a variable
   request.onreadystatechange = function() {
       if(request.readyState === XMLHttpRequest.DONE) {
