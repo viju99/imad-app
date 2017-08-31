08b8bc03e895ef36fb9dda0bababab07af93f595
viju99 <31035012+viju99@users.noreply.github.com>
2017-08-31 09:49:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -192,6 +192,7 @@ app.get('/counter', function(req,res){
 
 app.get('/:articleName', function (req, res) {
     //articleName will be article-one
+    console.log('reached articlce code');
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
